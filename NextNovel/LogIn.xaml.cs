5303beebca397e2d68a360aa6f948ee4a4a6190a
Saba <sberenji4567@conestogac.on.ca>
2024-03-18 23:58:45 -0400

    Created the sign up and the log in pages

---
+++ NextNovel/LogIn.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NextNovel
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LogIn_Click(object sender, RoutedEventArgs e) 
+        { 
+
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
