1f6a1e3edf1fcd2ada1d7d7cd3c24e42062be4e5
Sierra Erb <Serb2341@conestogac.on.ca>
2024-03-22 12:15:15 -0400

    Added Library and Book Info screens which use UserControl1 listbox item

---
+++ NextNovel/BookInfo.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NextNovel
+{
+    /// <summary>
+    /// Interaction logic for BookInfo.xaml
+    /// </summary>
+    public partial class BookInfo : Window
+    {
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public string ImagePath { get; set; }
+
+        public BookInfo(string image, string title, string author)
+        {
+            InitializeComponent();
+
+            ImagePath = image;
+            BookTitle = title;
+            Author = author;
+
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            double windowWidth = this.ActualWidth;
+            double windowHeight = this.ActualHeight;
+
+            LibraryScreen library = new LibraryScreen();
+
+
+            library.Width = windowWidth;
+            library.Height = windowHeight;
+
+
+            library.Show();
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NextNovel/LibraryScreen.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NextNovel
+{
+    /// <summary>
+    /// Interaction logic for LibraryScreen.xaml
+    /// </summary>
+    public partial class LibraryScreen : Window
+    {
+        public ObservableCollection<UserControl1> book { get; set; }
+
+
+        public LibraryScreen()
+        {
+            InitializeComponent();
+
+            book = new ObservableCollection<UserControl1>
+            {
+
+            new UserControl1
+                    {
+                        Image = "/Images/Book.jpg",
+                        Title = "Title 1",
+                        Author = "Author 1",
+
+                    },
+                new UserControl1
+                    {
+                         Image = "/Images/Book.jpg",
+                        Title = "Title 2",
+                        Author = "Author 2",
+
+                    },
+
+                new UserControl1
+                    {
+                       Image = "/Images/Book.jpg",
+                        Title = "Title 3",
+                        Author = "Auhtor 3",
+
+                    },
+                new UserControl1
+                    {
+                        Image = "/Images/Book.jpg",
+                        Title = "Title 4",
+                        Author = "Auhtor 4",
+
+                    },
+                new UserControl1
+                    {
+                        Image = "/Images/Book.jpg",
+                        Title = "Title 5",
+                        Author = "Auhtor 5",
+
+                    }
+            };
+
+
+           listBox.DataContext = book;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                double windowWidth = this.ActualWidth;
+                double windowHeight = this.ActualHeight;
+
+                UserControl1 selectedBook = (UserControl1)listBox.SelectedItem;
+
+
+                BookInfo bookScreen = new BookInfo(selectedBook.Image, selectedBook.Title, selectedBook.Author);
+
+
+                bookScreen.Width = windowWidth;
+                bookScreen.Height = windowHeight;
+
+
+                bookScreen.Show();
+                this.Close();
+            }
+        }
+    }
+}
